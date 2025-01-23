ca095ea912bcbff433b495835837478ff8256250
Stuart Forrester <stforrester@gmail.com>
2025-01-23 05:34:26 -0600

    completes lab and passes all tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = self.url
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        URL = self.url
+        response = requests.get(URL)
+        return response.json()
